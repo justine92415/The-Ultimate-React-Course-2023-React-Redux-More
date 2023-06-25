d680c26cda2ab88e9b1cb2fa28cd45e04990dd71
justine92415 <justine92415@gmail.com>
2023-06-25 16:30:47 +0800

    Let's Build a Steps Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,34 @@
+const message = [
+    'Learn React 🤗',
+    'Apply for jobs 😎',
+    'Invest your new income 💰',
+];
+
 function App() {
-    return <h1>Hello World</h1>;
+    const step = 1;
+
+    return (
+        <div className="steps">
+            <div className="numbers">
+                <div className={`${step >= 1 ? 'active' : ''}`}>1</div>
+                <div className={`${step >= 2 ? 'active' : ''}`}>2</div>
+                <div className={`${step >= 3 ? 'active' : ''}`}>3</div>
+            </div>
+
+            <p className="message">
+                Step {step}:{message[step - 1]}
+            </p>
+
+            <div className="buttons">
+                <button style={{ backgroundColor: '#7950f2', color: '#fff' }}>
+                    Previous
+                </button>
+                <button style={{ backgroundColor: '#7950f2', color: '#fff' }}>
+                    Next
+                </button>
+            </div>
+        </div>
+    );
 }
 
 export default App;
