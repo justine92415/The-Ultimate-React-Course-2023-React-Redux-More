c0a14c8340697c8ab9897c48315b5fac13cb4613
justine92415 <justine92415@gmail.com>
2023-06-25 13:04:09 +0800

    完成 CHALLENGE#1

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,45 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="card">
+            <Avatar photoPath="logo192.png" name="React Icon" />
+            <div className="data">
+                <Intro
+                    name="Jonas Schmedtmann"
+                    desc="Full-stack web developer and teacher at Udemy. When not coding
+                          or preparing a course, I like to play board games, to cook (and
+                          eat), or to just enjoy the Portuguese sun at the beach."
+                />
+                <SkillList skills={['HTML & CSS','JavaScript','Angular','Vue','React']}/>
+            </div>
+        </div>
+    );
+}
+
+function Avatar(props: { photoPath: string; name: string }) {
+    return (
+        <div className="avatar">
+            <img src={props.photoPath} alt={props.name}></img>
+        </div>
+    );
+}
+
+function Intro(props: { name: string; desc: string }) {
+    return (
+        <div>
+            <h1> {props.name} </h1>
+            <p> {props.desc} </p>
+        </div>
+    );
+}
+
+function SkillList(props: { skills: string[] }) {
+    return (
+        <ul className="skill-list">
+            {props.skills.map(skill => <li>{skill}</li>)}
+        </ul>
+    );
 }
 
 export default App;
