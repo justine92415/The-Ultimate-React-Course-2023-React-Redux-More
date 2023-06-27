9bfdaf638f9fa12579ca1c397f58db712febada9
justine92415 <justine92415@gmail.com>
2023-06-27 22:47:43 +0800

    完成 CHALLENGE#3

--- src/App.tsx
+++ src/App.tsx
@@ -1,100 +1,42 @@
 import './App.css';
-
-interface ISkill {
-    skill: string;
-    level: 'beginner' | 'intermediate' | 'advanced';
-    color: string;
-}
-
-enum SkillLevel {
-    beginner = '😐',
-    intermediate = '🤔',
-    advanced = '🤗',
-}
-
-const skills: ISkill[] = [
-    {
-        skill: 'HTML+CSS',
-        level: 'advanced',
-        color: '#2662EA',
-    },
-    {
-        skill: 'JavaScript',
-        level: 'advanced',
-        color: '#EFD81D',
-    },
-    {
-        skill: 'Web Design',
-        level: 'advanced',
-        color: '#C3DCAF',
-    },
-    {
-        skill: 'Git and GitHub',
-        level: 'intermediate',
-        color: '#E84F33',
-    },
-    {
-        skill: 'React',
-        level: 'advanced',
-        color: '#60DAFB',
-    },
-    {
-        skill: 'Svelte',
-        level: 'beginner',
-        color: '#FF3B00',
-    },
-];
+import { useState } from 'react';
 
 function App() {
     return (
-        <div className="card">
-            <Avatar photoPath="logo192.png" name="React Icon" />
-            <div className="data">
-                <Intro
-                    name="Jonas Schmedtmann"
-                    desc="Full-stack web developer and teacher at Udemy. When not coding
-                          or preparing a course, I like to play board games, to cook (and
-                          eat), or to just enjoy the Portuguese sun at the beach."
-                />
-                <SkillList skills={skills} />
-            </div>
+        <div className="App">
+            <Counter />
         </div>
     );
 }
 
-function Avatar(props: { photoPath: string; name: string }) {
-    return (
-        <img className="avatar" src={props.photoPath} alt={props.name}></img>
-    );
-}
+function Counter() {
+    const [count, setCount] = useState(0);
+    const [step, setStep] = useState(0);
 
-function Intro(props: { name: string; desc: string }) {
+    const date = new Date('june 21 2027');
+    date.setDate(date.getDate() + count);
     return (
         <div>
-            <h1> {props.name} </h1>
-            <p> {props.desc} </p>
-        </div>
-    );
-}
-
-function SkillList({ skills }: { skills: ISkill[] }) {
-    return (
-        <div className="skill-list">
-            {skills.map((skillObj) => (
-                <Skill skill={skillObj.skill} level={skillObj.level} color={skillObj.color} key={skillObj.skill} />
-            ))}
-        </div>
-    );
-}
-function Skill({skill, level, color}: {skill: string, level: string, color: string}) {
-    return (
-        <div className="skill" style={{ backgroundColor: color }}>
-            <span>{skill}</span>
-            <span>
-                {level === 'beginner' && SkillLevel.beginner}
-                {level === 'intermediate' && SkillLevel.intermediate}
-                {level === 'advanced' && SkillLevel.advanced}
-            </span>
+            <div>
+                <button onClick={() => setStep((c) => c - 1)}>-</button>
+                <span>Step: {step}</span>
+                <button onClick={() => setStep((c) => c + 1)}>+</button>
+            </div>
+            <div>
+                <button onClick={() => setCount((c) => c - step)}>-</button>
+                <span>Count: {count}</span>
+                <button onClick={() => setCount((c) => c + step)}>+</button>
+            </div>
+            <p>
+                <span>
+                    {!count
+                        ? 'Today is'
+                        : count > 0
+                        ? `${count} days from today is `
+                        : `${count} days ago was`}
+                </span>
+                <span>{date.toDateString()}</span>
+            </p>
         </div>
     );
 }
