fde198fcc1da1e614f085b40196ed1cd458a4cd3
justine92415 <justine92415@gmail.com>
2023-06-28 21:03:13 +0800

    Building a Form and Handling Submissions

--- src/App.tsx
+++ src/App.tsx
@@ -27,10 +27,23 @@ function Logo() {
 }
 
 function Form() {
+    function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+    }
+
     return (
-        <div className="add-form">
+        <form className="add-form" onSubmit={handleSubmit}>
             <h3>What do you need for your 😍 trip?</h3>
-        </div>
+            <select>
+                {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+                    <option value={num} key={num}>
+                        {num}
+                    </option>
+                ))}
+            </select>
+            <input type="text" placeholder="Item..." />
+            <button>Add</button>
+        </form>
     );
 }
 
