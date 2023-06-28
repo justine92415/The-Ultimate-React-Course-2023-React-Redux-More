c037d27480408933da07da93df6c0796f6a9921b
justine92415 <justine92415@gmail.com>
2023-06-28 20:53:31 +0800

    Rendering the Items List

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,16 @@
+const initialItems = [
+    { id: 1, description: 'Passports', quantity: 2, packed: false },
+    { id: 2, description: 'Socks', quantity: 12, packed: true },
+    { id: 3, description: 'Charger', quantity: 1, packed: false },
+];
+
+interface IItem {
+    id: number;
+    description: string;
+    quantity: number;
+    packed: boolean;
+}
+
 function App() {
     return (
         <div className="app">
@@ -22,7 +35,26 @@ function Form() {
 }
 
 function PackingList() {
-    return <div className="list">LIST</div>;
+    return (
+        <div className="list">
+            <ul>
+                {initialItems.map((item: IItem) => (
+                    <Item key={item.id} item={item} />
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+function Item({ item }: { item: IItem }) {
+    return (
+        <li>
+            <span style={item.packed ? { textDecoration: 'line-through' } : {}}>
+                {item.quantity} {item.description}
+            </span>
+            <button>❌</button>
+        </li>
+    );
 }
 
 function Stats() {
